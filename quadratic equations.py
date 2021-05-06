41f1e3f71d8ff56a5aae58c38f6881f0a4ec4c0a
Utkarsh <basilisk367@gmail.com>
2021-05-06 14:23:26 +0530

    new sha keys

--- quadratic equations.py
+++ quadratic equations.py
@@ -1,15 +1,14 @@
-a = float(input("Cofficient of x²:		"))
-b = float(input("Cofficient of x:  		"))
-c = float(input("Constant term:  		"))
+a = float(input("Cofficient of x²:	"))
+b = float(input("Cofficient of x:	"))
+c = float(input("Constant term:	"))
 d = (b**2) - (4*a*c)
 if d>=0:
 	r1 = (-b + (d**(1/2)))/2/a
 	r2 = (-b - (d**(1/2)))/2/a
-	print("Roots are:	" +str( r1 ) + " AnD " +str(r2))
+	print("Roots are:	" +str( r1 ) + " AND " +str(r2))
 if d<0: #makes non-real roots by taking roots as string!!
 	d = -d
 	d  = d**(1/2)
 	print("Caution: Non-Real Roots ahead!")
 	r1 = ("(" + str(-b) + " ± "   + str(d) + "i)/" + str(2*a))
-	print("Roots are:	" +str( r1 ))
-	
+	print("Roots are:	" +str(r1))	
